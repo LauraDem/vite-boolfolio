4fef6162ea72f96e3ff20c0522347409cafb5dc7
LauraDem <laura93dem@gmail.com>
2023-11-07 22:35:17 +0100

    Add: Axios - get api

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <script>
+import ProjectList from "./components/projects/ProjectList.vue";
+import axios from "axios";
+
 export default {
   data() {
-    return {};
+    return {
+      projects: [],
+    };
+  },
+
+  components: { ProjectList },
+
+  created() {
+    axios.get("http://127.0.0.1:8000/api/projects").then((response) => {
+      // console.log(response.data.data);
+      this.projects = response.data.data;
+    });
   },
 };
 </script>
 
 <template>
   <h1>Hello World</h1>
+  <hr />
+  <ProjectList />
 </template>
 
 <style lang="scss"></style>
---
+++ src/components/projects/ProjectList.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <h2>Project list</h2>
+</template>
+
+<style lang="scss"></style>
