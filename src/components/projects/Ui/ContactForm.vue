4cc4d42faeac320fa69e38092ba1be5e7f0820db
LauraDem <laura93dem@gmail.com>
2023-11-12 23:50:13 +0100

    Add: message controller

--- src/components/projects/Ui/ContactForm.vue
+++ src/components/projects/Ui/ContactForm.vue
@@ -1,5 +1,33 @@
 <script>
-export default {};
+import axios from "axios";
+import store from "../../../data/store";
+
+export default {
+  data() {
+    return {
+      email: "",
+      name: "",
+      message: "",
+    };
+  },
+  methods: {
+    sendMessage() {
+      if (!this.email || !this.name || !this.message) return;
+
+      const data = {
+        email: this.email,
+        name: this.name,
+        message: this.message,
+      };
+
+      // TODO: SISTEMA CORS
+      // TODO: AGGIUNGI PARTE FINALE URL
+      axios.post(store.api.baseUrl + "message", data).then((response) => {
+        console.log(response);
+      });
+    },
+  },
+};
 </script>
 
 <template>
@@ -7,6 +35,7 @@ export default {};
     <div class="mb-3 mt-5">
       <label for="contactform-email" class="form-label">Email address</label>
       <input
+        v-model="email"
         type="email"
         class="form-control"
         id="contactform-email"
@@ -17,6 +46,7 @@ export default {};
     <div class="mb-3">
       <label for="contactform-name" class="form-label">Nome</label>
       <input
+        v-model="name"
         type="text"
         class="form-control"
         id="contactform-name"
@@ -29,13 +59,16 @@ export default {};
         >Example textarea</label
       >
       <textarea
+        v-model="message"
         class="form-control"
         id="contactform-message"
         rows="3"
       ></textarea>
     </div>
 
-    <button class="btn btn-success">Invia messaggio</button>
+    <button class="btn btn-success" @click="sendMessage()">
+      <font-awesome-icon icon="paper-plane" class="me-2" /> Invia messaggio
+    </button>
   </div>
 </template>
 
